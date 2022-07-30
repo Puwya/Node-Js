336d47824a0b0d57fb47acfb8d0d2c9c6c83f7f7
Carlos Aguilera <18aguilerac@gmail.com>
2022-07-29 21:19:08 -0700

    added html static files

--- Node-Crash-Course/index.js
+++ Node-Crash-Course/index.js
@@ -1,7 +1,44 @@
-const Logger = require("./Logger");
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
-const logger = new Logger();
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    fs.readFile(
+      path.join(__dirname, "public", "index.html"),
+      (err, content) => {
+        if (err) throw err;
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(content);
+      }
+    );
+  }
+  if (req.url === "/about") {
+    fs.readFile(
+      path.join(__dirname, "public", "about.html"),
+      (err, content) => {
+        if (err) throw err;
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(content);
+      }
+    );
+  }
+  if (req.url === "/api/users") {
+    const users = [
+      {
+        name: "Bob",
+        age: 30,
+      },
+      {
+        name: "Tent",
+        age: 3,
+      },
+    ];
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(users));
+  }
+});
 
-logger.on("message", (data) => console.log("Called Listener:", data));
+const PORT = process.env.PORT || 3000;
 
-logger.log("Hello world");
+server.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
