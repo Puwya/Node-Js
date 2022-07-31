9cf9b700430de08bcdfa6c055e645dc450ec7759
Carlos Aguilera <18aguilerac@gmail.com>
2022-07-31 11:39:21 -0700

    custom middleware

--- Express-crash-course/index.js
+++ Express-crash-course/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const path = require('path');
+const members = require('./members');
+const Logger = require('./middleware/Logger');
 
 const app = express();
+
+// init custom middleware
+app.use(Logger);
+
+app.get('/api/members', (req, res) => res.json(members));
 
 // Set a static folder
 app.use(express.static(path.join(__dirname, 'public')));
